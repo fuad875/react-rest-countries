8836b7f24ae8552ba31d983443d0f0d218aa09d3
fuad875 <fuad1115279890@gmail.com>
2023-04-29 22:03:58 +0600

    bs

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import Countries from './components/Countries/Countries';
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
-  
+      <Header></Header>
       <Countries></Countries>
    
     </div>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import'./Header.css';
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome to my Rest Countries</h1>
+            <nav className='menu'>
+                <a href='/Home'>Home</a>
+                <a href='/Countries'>Country</a>
+                <a href='/about'>About</a>
+                
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
